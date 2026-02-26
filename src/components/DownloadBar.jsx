c49817adaa1d566b91f4f74c2382af20fd04453b
Michael <leeziyong0918@gmail.com>
2026-02-26 16:02:19 +0800

    Download Bar Changes

--- src/components/DownloadBar.jsx
+++ src/components/DownloadBar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function DownloadBar() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <a id="app-download-bar-a" href="#">
-        <div className="app-download-bar-div">
-            <p className="app-download-bar-p">Download App</p>
+        <div className="app-download-bar-div"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <p className="app-download-bar-p">{isHovered ? "Download CoDriver" : "Download App"}</p>
         </div>
     </a>
   );
